e5d9f0d0af039186020b97bd2ebb7f4736873d0e
Manikantha Nekkalapudi <manikantanekkalapudi@gmail.com>
2023-01-13 11:25:39 +0530

    Add LCMAndHCF class

---
+++ src/flowofprogram/LCMAndHCF.java
@@ -0,0 +1,31 @@
+package flowofprogram;
+
+import java.util.Scanner;
+
+public class LCMAndHCF {
+    public static void main(String[] args) {
+        Scanner num1 = new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int n1 = num1.nextInt();
+
+        Scanner num2 = new Scanner(System.in);
+        System.out.print("Enter 2nd number: ");
+        int n2 = num2.nextInt();
+
+        int temp, temp1, temp2, gcd, lcm;
+        temp1 = n1;
+        temp2 = n2;
+
+        while (temp2 != 0){
+            temp = temp2;
+            temp2 = temp1 % temp2;
+            temp1 = temp;
+        }
+
+        gcd = temp1;
+        lcm = (n1*n2)/gcd;
+
+        System.out.println("The HCF :"+gcd+" and LCM: "+lcm);
+
+    }
+}
